97989f8637d528aeb8b37e71d36ee5d77c84dfae
Blurryface1998 <83365791+Blurryface1998@users.noreply.github.com>
2022-12-20 10:30:58 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 */
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/interactive-rating-component'>
     <Routes>
       <Route index element={<App  />}  />
       <Route path='ThankYouPage' element={<ThankYou />} />
